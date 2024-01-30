3a8153f5b74c1a438e64473f458c1a3f3f38cf7f
Dallasocoggins <dallasocoggins@gmail.com>
2024-01-29 23:21:43 -0600

    Book Collection 2.0

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_19_153445) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_30_050636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_19_153445) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.float "price", default: 0.0
+    t.date "published_date", default: -> { "CURRENT_DATE" }
   end
 
 end
